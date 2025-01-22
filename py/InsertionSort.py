f304b6f104ca5df835b4160b0a3ba027c0a51c80
CalmaKarma <szlmail@qq.com>
2025-01-22 16:19:25 -0500

    completes quiz in Python

--- py/InsertionSort.py
+++ py/InsertionSort.py
@@ -1,5 +1,7 @@
 from typing import List
 
+DEBUG = False
+
 
 class InsertionSort:
     def __init__(self, array):
@@ -13,7 +15,20 @@ class InsertionSort:
 
     def insert(self, i):
         """Inserts the 'Transition element' into its correct position in the sorted portion of the array."""
-    #     TODO: TO BE IMPLEMENTED
+        for j in range(0, i):
+            if self.array[j] < self.array[i]:
+                continue
+            for k in range(i, j, -1):
+                self.swap(k, k - 1)
+            break
+
+        if DEBUG:
+            print(self.array)
 
     def swap(self, i, j):
         self.array[i], self.array[j] = self.array[j], self.array[i]
+
+
+if __name__ == '__main__':
+    DEBUG = True
+    InsertionSort([3, 3, 3, 1, 1, 2, 2]).sort()
